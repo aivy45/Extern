1ebc4227ae0b315a9d84d029efedb8009b24d96f
aivy45 <akshunverma45@gmail.com>
2022-11-17 23:41:33 +0530

    adding header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/header';
 function App() {
   return (
     <div className="App">
-     <h1>Akshun is here</h1>
+     <Header/>
     </div>
   );
 }
